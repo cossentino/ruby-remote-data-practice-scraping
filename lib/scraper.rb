4e6f159680b74028763099dfccd432638a7085c4
Ian Cossentino <icossentino@gmail.com>
2020-11-29 13:29:36 -0500

    commit tree fa8d24c91145e5e360b166b811204297d5991ff0

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+
+
+doc = Nokogiri::HTML(html)
+
+p doc.css(".text-4GLMvr")[1]
+
+
+
+
+#binding.pry
